f5af84be5e7502891a24ab5da6c4da18c276e88d
ArifMiah07 <arifmiah.me322@gmail.com>
2024-12-09 18:54:25 +0600

    prettier setup done

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
 console.log('hello world!');
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const b = 30;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const a = {
+  a: 30,
+  n: 459,
+};
