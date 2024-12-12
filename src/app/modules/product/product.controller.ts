f4b6417aa633f19ecb5e1a9fef88eadf03f2b181
ArifMiah07 <arifmiah.me322@gmail.com>
2024-12-13 00:51:41 +0600

    added query and error handler for get all bicycles

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,55 +1,64 @@
-import { Request, Response } from "express";
-import { productServices } from "./product.service";
-import productValidationSchema from "./product.validation";
+import { NextFunction, Request, Response } from 'express';
+import { productServices } from './product.service';
+import productValidationSchema from './product.validation';
 
+const createBicycle = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { bicycle: bicycleData } = req.body;
+    //data validation using Zod
+    const zodParsedData = productValidationSchema.parse(bicycleData);
+    const result = await productServices.createProductIntoDb(zodParsedData);
 
-
-const createProduct = async(req:Request, res: Response) =>{
-    try{
-        const {bicycle : bicycleData} = req.body; 
-        //data validation using Zod
-        const zodParsedData = productValidationSchema.parse(bicycleData);
-        const result = await productServices.createProductIntoDb(zodParsedData);
-
-        res.status(200).json({
-            success: true,
-            message: 'Bicycle created successfully',
-            data: result,
-        })
-    }catch(err : any){
-        res.status(500).json({
-            success: false,
-            message: err.message || "Something went wrong!",
-            error: err,
-        })
-    }
-}
+    res.status(200).json({
+      success: true,
+      message: 'Bicycle created successfully',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong!',
+      error: err,
+    });
+  }
+  next();
+};
 
 //get all products
 
-const getAllProducts = async(req: Request, res:Response)=>{
-    try{
-        const  result = await productServices.getAllProductsFromDb();
-
-        res.status(200).json({
-            success: true,
-            message: "Bicycles retrieved successfully",
-            data: result,
-        })
-    }catch(err : any){
-        res.status(500).json({
-            success: false,
-            message: err.message || "Something went wrong",
-            error: {
-                name: err.name || "Error",
-                details: err.details,
-            },
-        })
+const getAllBicycles = async (req: Request, res: Response, next: NextFunction): Promise<Response |undefined> => {
+  try {
+    const { searchTerm } = req.query;
+    const result = await productServices.getAllProductsFromDb(
+      searchTerm as string,
+    );
+    if (!result.success) {
+      return res.status(404).json({
+        success: false,
+        message: result.message, // "No products found matching your search criteria."
+        data: result.data,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'Bicycles retrieved successfully',
+        data: result,
+      });
     }
-}
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong',
+      error: {
+        name: err.name || 'Error',
+        details: err.details,
+      },
+    });
+  }
+  next()
+};
 
 export const productController = {
-    createProduct,
-    getAllProducts,
-
-}
+  createProduct: createBicycle,
+  getAllProducts: getAllBicycles,
+};
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -3,19 +3,30 @@ import { Product } from "../product.model";
 import { TProduct } from "./product.interface";
 
 
-const createProductIntoDb = async (productData : TProduct)=>{
+const createBicyclesIntoDb = async (productData : TProduct)=>{
     const result = await Product.create(productData);
 
     return result;
 };
 
-const getAllProductsFromDb = async()=>{
-    const result = Product.find();
-    return result;
+const getAllBicyclesFromDb = async(searchTerm? : string)=>{
+    console.log("Search Term:", searchTerm);
+    let filter = {};
+    if(searchTerm){
+        filter = {$or: [{name:  { $regex: searchTerm, $options: 'i' }},{brand:  { $regex: searchTerm, $options: 'i' }}, {type:  { $regex: searchTerm, $options: 'i' }}]}//using $or operator for one matching query value;
+    }
+    const result = await Product.find(filter).sort({ createdAt: -1 });
+    if (result.length === 0) {
+        return { success: false, message: "No products found matching your search criteria.", data: [] };
+      }
+    
+      return { success: true, message: "Bicycles retrieved successfully", data: result };
+    // return result;
+    
 }
 
 
 export const productServices = {
-    createProductIntoDb,
-    getAllProductsFromDb,
+    createProductIntoDb: createBicyclesIntoDb,
+    getAllProductsFromDb: getAllBicyclesFromDb,
 }
